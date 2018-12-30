7dedf7a56f7d01bea8638854273249db6b10667e
Kevin van Hoorn <kevin.van.hoorn@me.com>
2018-12-30 13:53:24 +0100

    changed component class description

--- custom_components/sensor/cure_waste.py
+++ custom_components/sensor/cure_waste.py
@@ -1,6 +1,8 @@
 """
+Support for Cure waste xml reading
+
 For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/sensor.mind/
+https://github.com/kvanhoorn/hass/
 """
 import os
 import logging
